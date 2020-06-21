f7f8e59c9e58d62884adbbb13c80e465fefea3bd
qiaopeng <501220307@qq.com>
2020-06-21 22:58:15 +0800

    Initial commit

---
+++ src/test/java/com/example/mybatis/MybatisApplicationTests.java
@@ -0,0 +1,9 @@
+package com.example.mybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisApplicationTests {
+
+}
